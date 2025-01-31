5a0f0c45682b74746541e24f4c3ae12784e38b71
Reeyyyh <rhnlnv@gmail.com>
2025-02-01 02:27:13 +0700

    make new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Header from "./components/Header"
+
 
 function App() {
 
   return (
     <>
-    <h1 className='font-bold text-center text-5xl'>Personal Portofolio</h1>
+      <Header />
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return(
+        <>
+        <div className="bg-red-500">
+            <h1 className="font-bold text-3xl">My Personal Portofolio profile</h1>
+        </div>
+        </>
+    )
+}
+
+export default Header
